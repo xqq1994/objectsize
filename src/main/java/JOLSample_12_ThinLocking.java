cbd777d8611c89bfc26f7f70cc7580bde83caa23
qiangqiang.xiao <qiangqiang.xiao@591hx.com>
2020-11-02 15:10:16 +0800

    5th commit

--- src/main/java/JOLSample_12_ThinLocking.java
+++ src/main/java/JOLSample_12_ThinLocking.java
@@ -22,7 +22,6 @@ public class JOLSample_12_ThinLocking {
         AiStockDto aiStockDto = new AiStockDto();
         String s = JSON.toJSONString(aiStockDto);
         System.out.println(ClassLayout.parseInstance(s).toPrintable());
-        System.out.println();
     }
 
     public static class A {
